df7041ea95e85ee1f2e3cffda38c8e11052ce53d
Saurabh Krishan Gulab <saurabhgulab@gmail.com>
2024-09-06 09:47:27 +0530

    Code Stucture Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,9 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;Love&nbsp;Chatting with {` `}
+            Upgrade To Electric {` `}
             <span className="inline-block relative">
-              Saurabh Krishan Gulab 🐰{" "}
+              #DontBeFuelish
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -33,7 +33,14 @@ import {
   sliders04,
   telegram,
   twitter,
-  yourlogo,
+  icici,
+  bajaj,
+  cfc,
+  ezfinanz,
+  zest,
+  kotak,
+  vidharbhkonkan,
+  loantap,
 } from "../assets";
 
 export const navigation = [
@@ -75,7 +82,16 @@ export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
 
 export const notificationImages = [notification4, notification3, notification2];
 
-export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+export const companyLogos = [
+  icici,
+  bajaj,
+  kotak,
+  ezfinanz,
+  zest,
+  vidharbhkonkan,
+  loantap,
+  cfc,
+];
 
 export const brainwaveServices = [
   "Photo generating",
